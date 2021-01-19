de21edf2f89537721b15ed598118ab6e4b9fcc95
Michael Kennedy <mikeckennedy@gmail.com>
2021-01-18 16:45:30 -0800

    Starter code for chapter 5.

---
+++ code/ch05-database/api/order_api.py
@@ -0,0 +1,15 @@
+import flask
+
+from models.order import CakeOrder
+
+blueprint = flask.Blueprint('order_api', 'order_api')
+
+
+@blueprint.route('/api/order', methods=['POST'])
+def order():
+    data = flask.request.get_json(force=True)
+    cake_order = CakeOrder(**data)
+
+    print(cake_order)
+
+    return {"received": cake_order.dict()}
