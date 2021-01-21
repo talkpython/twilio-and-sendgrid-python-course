f4b971518d3e829cb94a8573653384f65f6e3f21
Michael Kennedy <mikeckennedy@gmail.com>
2021-01-21 15:26:32 -0800

    Add the "bare" order ORM class.

---
+++ code/ch05-database/db/order.py
@@ -0,0 +1,15 @@
+import datetime
+
+
+class Order:
+    id: int
+    created_date: datetime.datetime
+    fulfilled_date: datetime.datetime
+
+    size: str
+    flavour: str
+    topping: str
+    frosting: str
+    price: float
+
+    user_id: int
