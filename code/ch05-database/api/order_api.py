ca38d7f336291de3d2382941a97ee8d6d0758ff6
Michael Kennedy <mikeckennedy@gmail.com>
2021-01-21 16:19:32 -0800

    Saving an order and user to the DB.

--- code/ch05-database/api/order_api.py
+++ code/ch05-database/api/order_api.py
@@ -1,6 +1,7 @@
 import flask
 
 from models.order import CakeOrder
+from service import user_service
 
 blueprint = flask.Blueprint('order_api', 'order_api')
 
@@ -10,8 +11,14 @@ def order():
     data = flask.request.get_json(force=True)
     cake_order = CakeOrder(**data)
 
-    # TODO: Create or update user & record order
+    # Create or update user & record order
+    db_order = user_service.record_order(
+        cake_order.customer,
+        cake_order.cake,
+        cake_order.price)
+
     # TODO: Send email receipt + invoice
+
     # TODO: Return order details to user via Studio
 
     print(cake_order)
