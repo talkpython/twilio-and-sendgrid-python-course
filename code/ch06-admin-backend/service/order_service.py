f1a2dcdafb05c12bcfca9c29fda68b3b016f73f6
Michael Kennedy <mikeckennedy@gmail.com>
2021-02-22 16:08:52 -0800

    Order fulfillment done in admin backend.

--- code/ch06-admin-backend/service/order_service.py
+++ code/ch06-admin-backend/service/order_service.py
@@ -1,4 +1,5 @@
-from typing import List
+import datetime
+from typing import List, Optional
 
 from sqlalchemy.orm import subqueryload
 
@@ -7,11 +8,28 @@ from db.session import SessionContext
 
 
 def all_cake_orders() -> List[Order]:
-    with SessionContext(commit_on_success=True) as ctx:
-
-        orders = ctx.session.query(Order)\
+    with SessionContext() as ctx:
+        orders = ctx.session.query(Order) \
             .options(subqueryload(Order.user)) \
-            .order_by(Order.created_date.desc())\
+            .order_by(Order.created_date.desc()) \
             .all()
 
     return orders
+
+
+def find_order_by_id(order_id: int) -> Optional[Order]:
+    with SessionContext() as ctx:
+        order = ctx.session.query(Order).filter(Order.id == order_id).first()
+        return order
+
+
+def fulfill_order(order_id: int) -> bool:
+    with SessionContext(commit_on_success=True) as ctx:
+        order = ctx.session.query(Order).filter(Order.id == order_id).first()
+        if not order:
+            return False
+
+        if order.fulfilled_date:
+            return True
+
+        order.fulfilled_date = datetime.datetime.now()
--- code/ch06-admin-backend/views/admin.py
+++ code/ch06-admin-backend/views/admin.py
@@ -9,3 +9,17 @@ blueprint = flask.Blueprint('admin', 'admin')
 def index():
     orders = order_service.all_cake_orders()
     return flask.render_template('admin/index.html', orders=orders)
+
+
+@blueprint.route('/admin/fulfill/<order_id>')
+def fulfill(order_id: int):
+    order = order_service.find_order_by_id(order_id)
+    if not order:
+        return flask.abort(404)
+
+    order_service.fulfill_order(order_id)
+
+    # TODO: Send whatsapp message to notify the customer!
+
+    return flask.redirect('/admin')
+
