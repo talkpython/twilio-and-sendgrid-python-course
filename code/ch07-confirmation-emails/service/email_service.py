39f77fb93eb13a1f0b4f060334d922ca38d719cf
Michael Kennedy <mikeckennedy@gmail.com>
2021-02-25 15:06:32 -0800

    Format the inline text for better reading.

--- code/ch07-confirmation-emails/service/email_service.py
+++ code/ch07-confirmation-emails/service/email_service.py
@@ -13,18 +13,21 @@ def send_cake_order_receipt(order: Order):
     from_email = sendgrid.From('michael@talkpython.fm', "Michael Kennedy")
     to_email = sendgrid.To(order.user.email, order.user.name)
     subject = sendgrid.Subject("Your order receipt from Cloud City Cakes")
-    html = "<h1>Your Receipt</h1>\n<br>\n<br>Thanks for ordering your " \
-           f"{order.size} {order.flavour} cake."
-    text = "Your Receipt\n\nThanks for ordering your " \
-           f"{order.size} {order.flavour} cake."
 
-    message = sendgrid.Mail(from_email, to_email, subject, text, html)
+    html = "<h1>Your Receipt</h1>" \
+           "\n<br>" \
+           f"Thanks for ordering your {order.size} {order.flavour} cake."
 
-    # TODO: Build PDF invoice
+    text = "Your Receipt" \
+           "\n" \
+           f"Thanks for ordering your {order.size} {order.flavour} cake."
+
     # TODO: Generate HTML content
+    # TODO: Build PDF invoice
     # TODO: Attach the invoice to the email
 
     # Send the email
+    message = sendgrid.Mail(from_email, to_email, subject, text, html)
     response: python_http_client.client.Response = client.send(message)
 
     if response.status_code not in {200, 201, 202}:
