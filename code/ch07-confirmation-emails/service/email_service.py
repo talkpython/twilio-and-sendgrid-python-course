c04517d4581ed967231bbc06a8938b73d86620e1
Michael Kennedy <mikeckennedy@gmail.com>
2021-02-25 15:13:02 -0800

    Using Jinja templates for HTML content.

--- code/ch07-confirmation-emails/service/email_service.py
+++ code/ch07-confirmation-emails/service/email_service.py
@@ -1,5 +1,7 @@
+from pathlib import Path
 from typing import Optional
 
+import jinja2
 import python_http_client
 import sendgrid
 
@@ -14,9 +16,7 @@ def send_cake_order_receipt(order: Order):
     to_email = sendgrid.To(order.user.email, order.user.name)
     subject = sendgrid.Subject("Your order receipt from Cloud City Cakes")
 
-    html = "<h1>Your Receipt</h1>" \
-           "\n<br>" \
-           f"Thanks for ordering your {order.size} {order.flavour} cake."
+    html = build_html('email/testemail.html', {'order': order})
 
     text = "Your Receipt" \
            "\n" \
@@ -25,6 +25,9 @@ def send_cake_order_receipt(order: Order):
     # TODO: Generate HTML content
     # TODO: Build PDF invoice
     # TODO: Attach the invoice to the email
+    print("Would send HTML:")
+    print(html)
+    return
 
     # Send the email
     message = sendgrid.Mail(from_email, to_email, subject, text, html)
@@ -34,3 +37,14 @@ def send_cake_order_receipt(order: Order):
         raise Exception(f"Error sending email: {response.status_code}")
 
     print(f"Sent email successfully: Order {order.id} to {order.user.name} at {order.user.email}")
+
+
+def build_html(template_file: str, data: dict) -> str:
+    template_folder = str(Path(__file__).parent.parent / "templates")
+    loader = jinja2.FileSystemLoader(template_folder)
+    env = jinja2.Environment(loader=loader)
+
+    template: jinja2.Template = loader.load(env, template_file, None)
+    html = template.render(**data)
+
+    return html
