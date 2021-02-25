e4be53098d20449b6dd01244724034ae81defd75
Michael Kennedy <mikeckennedy@gmail.com>
2021-02-25 15:44:34 -0800

    Generating invoice PDF

--- code/ch07-confirmation-emails/service/email_service.py
+++ code/ch07-confirmation-emails/service/email_service.py
@@ -1,8 +1,11 @@
+import base64
+import uuid
 from pathlib import Path
 from typing import Optional
 
 import html2text
 import jinja2
+import pdfkit
 import python_http_client
 import sendgrid
 
@@ -21,7 +24,8 @@ def send_cake_order_receipt(order: Order):
     text = html2text.html2text(html)
 
     invoice_html = build_html('email/invoice.html', {'order': order})
-    print(invoice_html)
+    pdf = build_pdf(invoice_html)
+    print(pdf)
     return
 
     # TODO: Build PDF invoice
@@ -35,6 +39,19 @@ def send_cake_order_receipt(order: Order):
         raise Exception(f"Error sending email: {response.status_code}")
 
     print(f"Sent email successfully: Order {order.id} to {order.user.name} at {order.user.email}")
+
+
+def build_pdf(html: str) -> str:
+    # Requires install from https://wkhtmltopdf.org/ in addition to pdfkit.
+    temp_file = Path(__file__).parent.parent / (str(uuid.uuid4()) + ".pdf")
+
+    pdfkit.from_string(html, str(temp_file))
+    pdf_bytes = temp_file.read_bytes()
+
+    temp_file.unlink()
+
+    encoded_pdf = base64.b64encode(pdf_bytes).decode('ascii')
+    return encoded_pdf
 
 
 def build_html(template_file: str, data: dict) -> str:
